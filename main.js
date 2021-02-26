033ba5be4f48ee188e02ea00314e7c285e8f1e2f
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-25 18:27:00 -0600

    Coding Challenges

--- main.js
+++ main.js
@@ -12,6 +12,73 @@
 
 
 
+// Search and Replace
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+//
+// First argument is the sentence to perform the search and replace on.
+//
+// Second argument is the word that you will be replacing (before).
+//
+// Third argument is what you will be replacing the second argument with (after).
+//
+// Note
+// Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+
+
+//
+// function myReplace(str, before, after){
+//   if(/^[A-Z]/.test(before)){
+//     after = after[0].toUpperCase() + after.substring(1)
+//   }else{
+//     after = after[0].toLowerCase() + after.substring(1)
+//   }
+//   return str.replace(before, after)
+// }
+//
+//
+// console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
+// // should return "He is Sitting on the couch"
+
+
+// Pig Latin
+// Pig Latin is a way of altering English Words. The rules are as follows:
+//
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+//
+// - If a word begins with a vowel, just add "way" at the end.
+//
+// Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+
+
+// function translatePigLatin(str){
+//   if(str[0] === 'a' || str[0] === 'e' || str[0] === 'i' || str[0] === 'o' || str[0] === 'u'){
+//     return str + 'way'
+//   }else{
+//     const elimCons = str.replace(/^[^aeiou]+/, '')
+//     return elimCons + str.match(/^[^aeiou]+/).join('') + 'ay'
+//   }
+// }
+
+
+// console.log(translatePigLatin("schwartz"))
+// should return "artzschway"
+// console.log(translatePigLatin('algorithm'))
+// should return "algorithmway
+
+
+// Spinal Tap Case
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+// function spinalCase(str){
+//   let noCamel = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+//   return noCamel.replace(/[^a-z0-9]/gi,'-').toLowerCase()
+// }
+//
+// console.log(spinalCase('thisIsSpinalTap'))
+
+// console.log(spinalCase('The_Andy_Griffith_Show'))
+// spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
+
 
 // Wherefore art thou
 // Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
