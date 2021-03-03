ba29ea186fa3fcd647a0165869d5dfe9c2a540c7
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-03 13:48:43 -0600

    Interm. Coding Challenges

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 
+// Palindrome Checker
+// Return true if the given string is a palindrome. Otherwise, return false.
+//
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+//
+// Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+//
+// We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+//
+// We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+
+// function palindrome(str){
+//   let newStr = str.trim().replace(/[^a-z0-9]/gi, '').toLowerCase()
+//   let pali = newStr.split('').reverse().join('')
+//   return newStr === pali
+// }
+//
+// console.log(palindrome("_eye"))
+
 
 // Arguments Optional
 // Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
