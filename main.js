416eeaee956ce84324e6fe77bb7eb15c0a467b0a
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-04 11:26:12 -0600

    Interm. Coding Challenges

--- main.js
+++ main.js
@@ -8,32 +8,13 @@
 // All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
 
-// function rot13(str){
-// str = str.split(' ').join('-')
-// let numArr = []
-// let wordsArr = []
-// let newWordsArr = []
-// for(let i = 0; i < str.length; i++){
-//   numArr.push(str.charCodeAt(i))
-// }
-// let shifted = numArr.map(x => x + 13)
-//
-//
-// for(let j = 0; j < shifted.length; j++){
-//   if(shifted[j] <= 90){
-//     wordsArr.push(shifted[j])
-//   }else{
-//     wordsArr.push(shifted[j] - 26)
-//   }
-// }
-// wordsArr.forEach(x => newWordsArr.push(String.fromCharCode(x)))
-//   return newWordsArr.join('').replace(/:/g,' ')
-//
-// return shifted
+// function rot13(str) {
+//   return str.replace(/[A-Z]/g, L =>
+//     String.fromCharCode((L.charCodeAt(0) % 26) + 65)
+//   );
 // }
 //
-// console.log(rot13("SERR CVMMN!"))
-
+// console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
 
 
 // Roman Numeral Converter
