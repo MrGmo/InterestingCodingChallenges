358265c6f47ed974176476625235ce24848d1bc7
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-20 15:06:41 -0500

    Coding Challenge

--- index.js
+++ index.js
@@ -757,13 +757,13 @@
 // Please create a function that takes in the name of a person. This function should add this person to a names list and let them know their position on the list. The names list should be created outside of the function.
 
 // let bigList = ['James', 'Karen', 'Mike', 'Sara']
-//
-// function personName(bozo){
-//   bigList.push(bozo)
-//   console.log(`${bozo} you are #${bigList.length} in the array`)
+
+// function personName(var){
+//   bigList.push(var)
+//   console.log(`${var} you are #${bigList.length} in the array`)
 // }
-//
-// personName('Willy')
+
+// personName('Max')
 
 
 // Please create a function that takes in an array of numbers and returns a new array that only contains odd numbers.
